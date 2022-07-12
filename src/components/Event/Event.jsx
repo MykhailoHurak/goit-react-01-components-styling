476853d786dc4f8a7bc4e16d8f2de51aec046297
mikegurak <92752760+mikegurak@users.noreply.github.com>
2022-07-12 11:03:40 +0300

    create constants iconSize

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Event.module.css';
+import { iconSize } from 'constants';
 // import { formatEventStart } from 'utilsHelpers/formatEventStart';
 // import { formatEventDuration } from 'utilsHelpers/formatEventDuration';
 import { formatEventStart, formatEventDuration } from 'utilsHelpers';
@@ -12,19 +13,19 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
         <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
             <p className={css.info}>
-                <FaMapMarkerAlt className={css.icon} size={16} />
+                <FaMapMarkerAlt className={css.icon} size={iconSize.sm} />
                 {location}
             </p>
             <p className={css.info}>
-                <FaUserAlt className={css.icon} size={16} />
+                <FaUserAlt className={css.icon} size={iconSize.sm} />
                 {speaker}
             </p>
             <p className={css.info}>
-                <FaCalendarAlt className={css.icon} size={16} />
+                <FaCalendarAlt className={css.icon} size={iconSize.sm} />
                 {formatedStart}
             </p>
             <p className={css.info}>
-                <FaClock className={css.icon} size={16} />
+                <FaClock className={css.icon} size={iconSize.sm} />
                 {duration}
             </p>
             <span className={`${css.chip} ${css[type]}`}>{type}</span>
